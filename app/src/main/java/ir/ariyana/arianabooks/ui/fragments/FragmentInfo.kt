22e55876e4340d8a6a7ae6f8a7c00cbd18dd6200
Moein Latifi <moeinl1090@protonmail.com>
2022-05-10 19:32:47 +0430

    added fragment info

---
+++ app/src/main/java/ir/ariyana/arianabooks/ui/fragments/FragmentInfo.kt
@@ -0,0 +1,26 @@
+package ir.ariyana.arianabooks.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ir.ariyana.arianabooks.databinding.FragmentInfoBinding
+
+class FragmentInfo: Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
