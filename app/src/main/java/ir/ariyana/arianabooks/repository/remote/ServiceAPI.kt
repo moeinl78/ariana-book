0f3509fb26afd17cb2e986e2d2c9c69770abd996
Moein Latifi <moeinl1090@protonmail.com>
2022-05-10 13:45:54 +0430

    added api service

---
+++ app/src/main/java/ir/ariyana/arianabooks/repository/remote/ServiceAPI.kt
@@ -0,0 +1,14 @@
+package ir.ariyana.arianabooks.repository.remote
+
+import ir.ariyana.arianabooks.repository.model.BooksOverviewResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ServiceAPI {
+
+    @GET("lists/overview.json")
+    suspend fun getBooksOverview(
+        @Query("apiKey") apiKey: String = "CFGmGBqGHX5bfNFxZZxh4dtziwgl6PXa"
+    ): Response<BooksOverviewResponse>
+}
