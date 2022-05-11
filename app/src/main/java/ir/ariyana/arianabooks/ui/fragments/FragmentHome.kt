0eaf28098cc1e5a02ba5b21037b9d79b15b18e4d
Moein Latifi <moeinl1090@protonmail.com>
2022-05-11 22:44:35 +0430

    added adapter and view model to home fragment

--- app/src/main/java/ir/ariyana/arianabooks/ui/fragments/FragmentHome.kt
+++ app/src/main/java/ir/ariyana/arianabooks/ui/fragments/FragmentHome.kt
@@ -5,11 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ir.ariyana.arianabooks.databinding.FragmentHomeBinding
+import ir.ariyana.arianabooks.repository.model.Book
+import ir.ariyana.arianabooks.ui.adapter.AdapterBook
+import ir.ariyana.arianabooks.ui.main.MainViewModel
 
 class FragmentHome: Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: AdapterBook
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +32,25 @@ class FragmentHome: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel
+            .getBooksOverview()
 
+        adapter = AdapterBook()
+
+        viewModel
+            .response
+            .observe(viewLifecycleOwner) { item ->
+                val books = mutableListOf<Book>()
+                item.data?.let { res ->
+                    res.results.lists[0].books.map { book ->
+                        books.add(book)
+                    }
+                }
+                adapter.submitList(books)
+            }
+
+        binding.homeRecyclerViewBook.adapter = adapter
+        binding.homeRecyclerViewBook.layoutManager =
+            GridLayoutManager(binding.root.context, 2)
     }
 }
